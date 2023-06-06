299d58571d285125c79499a63b3044437a195139
Johndoe <56298669+togane@users.noreply.github.com>
2023-06-06 18:39:14 +0900

    fix entry point

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+
+module.exports = async function(env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.entry = './AppEntry.js';
+
+  return config;
+};
