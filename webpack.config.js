bfbe6bc8e3bd5b547742e044ab5b3a3f6b9f3a37
Johndoe <56298669+togane@users.noreply.github.com>
2023-06-06 19:10:58 +0900

    Add webpack alias for src directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const path = require('path');
 
 module.exports = async function(env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
 
   config.entry = './AppEntry.js';
+
+  if (!config.resolve) {
+    config.resolve = {};
+  }
+
+  if (!config.resolve.alias) {
+    config.resolve.alias = {};
+  }
+
+  config.resolve.alias['@'] = path.resolve(__dirname, 'src/');
 
   return config;
 };
