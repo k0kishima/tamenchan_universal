f468819570ed1a9af46afbdda82f7814a8388154
Johndoe <56298669+togane@users.noreply.github.com>
2023-06-10 20:12:39 +0900

    Add some rome-ignore

--- src/components/CheatSheet/index.tsx
+++ src/components/CheatSheet/index.tsx
@@ -9,8 +9,9 @@ type Props = {
 export const CheatSheet: React.FC<Props> = ({ handAndWinTilesPairs }: Props) => {
   return (
     <View style={styles.container}>
-      {handAndWinTilesPairs.map(([hand, winTiles], index) => (
-        <View key={index} style={styles.row}>
+      {handAndWinTilesPairs.map(([hand, winTiles], i) => (
+        // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+        <View key={i} style={styles.row}>
           <View style={[styles.column, { flex: 7 }]}>
             <Hand color="m" number={hand} containerWidthPercent={70} />
           </View>
