1d4227344651a1af6b8271882ee91f9bcdaf0672
Johndoe <56298669+togane@users.noreply.github.com>
2023-06-10 20:23:34 +0900

    Fix Tile component

--- src/components/Tile/index.tsx
+++ src/components/Tile/index.tsx
@@ -9,8 +9,45 @@ type Props = {
   imageStyle?: ImageStyle;
 };
 
+type TileImages = Record<TileColor, Record<TileNumber, ReturnType<typeof require>>>;
+const tileImages: TileImages = {
+  m: {
+    1: require("@/assets/images/tiles/m1.gif"),
+    2: require("@/assets/images/tiles/m2.gif"),
+    3: require("@/assets/images/tiles/m3.gif"),
+    4: require("@/assets/images/tiles/m4.gif"),
+    5: require("@/assets/images/tiles/m5.gif"),
+    6: require("@/assets/images/tiles/m6.gif"),
+    7: require("@/assets/images/tiles/m7.gif"),
+    8: require("@/assets/images/tiles/m8.gif"),
+    9: require("@/assets/images/tiles/m9.gif"),
+  },
+  p: {
+    1: require("@/assets/images/tiles/p1.gif"),
+    2: require("@/assets/images/tiles/p2.gif"),
+    3: require("@/assets/images/tiles/p3.gif"),
+    4: require("@/assets/images/tiles/p4.gif"),
+    5: require("@/assets/images/tiles/p5.gif"),
+    6: require("@/assets/images/tiles/p6.gif"),
+    7: require("@/assets/images/tiles/p7.gif"),
+    8: require("@/assets/images/tiles/p8.gif"),
+    9: require("@/assets/images/tiles/p9.gif"),
+  },
+  s: {
+    1: require("@/assets/images/tiles/p1.gif"),
+    2: require("@/assets/images/tiles/p2.gif"),
+    3: require("@/assets/images/tiles/p3.gif"),
+    4: require("@/assets/images/tiles/p4.gif"),
+    5: require("@/assets/images/tiles/p5.gif"),
+    6: require("@/assets/images/tiles/p6.gif"),
+    7: require("@/assets/images/tiles/p7.gif"),
+    8: require("@/assets/images/tiles/p8.gif"),
+    9: require("@/assets/images/tiles/p9.gif"),
+  },
+};
+
 export const Tile: React.FC<Props> = ({ color, number, imageStyle = defaultStyles.image }: Props) => {
-  return <Image style={imageStyle} source={require(`@/assets/images/tiles/${color}${number}.gif`)} />;
+  return <Image style={imageStyle} source={tileImages[color][number]} />;
 };
 
 const defaultStyles = StyleSheet.create({
